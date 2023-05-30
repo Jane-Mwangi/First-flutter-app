d8cc0338fd63638b17aa7b217584d28925b175b2
Jane Mwangi <janiegrace@gmail.com>
2023-05-30 10:49:02 +0300

    Works with assets and pubspec file. Add icons.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('hello flutter'),
+    home: Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+      title: Text('I am Loved'),
+        backgroundColor: Colors.cyanAccent,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+
     ),
+
   ));
 }
